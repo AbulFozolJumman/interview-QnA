544ee737b95737b3b76aeb9743c846e4a9be011a
AbulFozolJumman <abulfozoljumman@gmail.com>
2024-02-21 11:12:53 +0600

    Git cloning done

---
+++ src/pages/ES6Questions.jsx
@@ -0,0 +1,11 @@
+import Container from "../components/Shared/Container/Container";
+
+const ES6Questions = () => {
+    return (
+        <Container>
+            ES6 questions
+        </Container>
+    );
+};
+
+export default ES6Questions;
---
+++ src/pages/NextJSQuestions.jsx
@@ -0,0 +1,11 @@
+import Container from "../components/Shared/Container/Container";
+
+const NextJSQuestions = () => {
+    return (
+        <Container>
+            Next JS questions
+        </Container>
+    );
+};
+
+export default NextJSQuestions;
